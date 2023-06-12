49d247ffa8be8be2001a92056c13dbd6e937732c
Lyubov Danishek <122434359+Lyubov4@users.noreply.github.com>
2023-06-12 15:10:54 +0800

    Решила задачу 1_module 1_task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
-function factorial(n) {
-  // ваш код...
-}
+
+  function factorial(n) {
+    let resp = 1;
+    let i = 1;
+    while (i<=n) {
+    resp = resp * i;
+    i++;
+    }
+    return resp
+  }
+  console.log (factorial (0));
+  console.log (factorial (1));
+  console.log (factorial (3));
+  console.log (factorial (5));  
+
