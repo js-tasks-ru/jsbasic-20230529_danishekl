e35fd5e727f36bd40d71f5f7f09ef64d1497ed44
Lyubov Danishek <122434359+Lyubov4@users.noreply.github.com>
2023-06-21 18:46:38 +0800

    Решила задачу 2_module 2_task

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,13 @@
-function isEmpty(obj) {
-  // ваш код...
+function isEmpty(schedule) {
+  for (let key in schedule) {
+    return false;
+  }
+  return true;
 }
+
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "подъём";
+alert( isEmpty(schedule) ); // false
+
