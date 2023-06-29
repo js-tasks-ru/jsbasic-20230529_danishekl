b48b16d3e8724e24ba795eead15e46dd6998ff15
Lyubov Danishek <122434359+Lyubov4@users.noreply.github.com>
2023-06-29 16:17:21 +0800

    Исправила задачу 3_module 2_task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,10 +1,20 @@
 let arr = [5, 3, 8, 1];
 
 function filterRange(arr, a, b) {
-  return arr.filter (item => (a <= item && item <= b));
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= a && arr[i] <= b) {
+      result.push(arr[i]);
+    }
+  }
+
+  return result;
 }
 
 let filtered = filterRange(arr, 1, 4);
 
-console.log(filtered); 
-console.log(arr); 
+console.log(filtered);
+console.log(arr);
+
+
