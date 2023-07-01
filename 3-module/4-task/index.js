8768a45022ce928781bf68d58c17a13e799733f0
Lyubov Danishek <122434359+Lyubov4@users.noreply.github.com>
2023-07-01 10:51:47 +0800

    Решила задачу 3_module 4_task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let filtUs = users.filter(user => user.age <= age);
+  let result = '';
+  filtUs.forEach((user, index) => {
+    result += `${user.name}, ${user.balance}`;
+    if (index !== filtUs.length - 1) {
+      result += '\n';
+    }
+  });
+  return result;
 }
