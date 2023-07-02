037d2571e39bc5d619629f20a300c192b27f91f3
Lyubov Danishek <122434359+Lyubov4@users.noreply.github.com>
2023-07-02 19:46:59 +0800

    Решила задачу 3_module 5_task

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,17 @@
 function getMinMax(str) {
-  // ваш код...
+  const elements = str.split(' ');
+  const numbers = [];
+
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    const number = parseFloat(element);
+    if (!isNaN(number)) {
+      numbers.push(number);
+    }
+  }
+
+const min = Math.min.apply(null, numbers);
+const max = Math.max.apply(null, numbers);
+  return { min, max };
 }
+
