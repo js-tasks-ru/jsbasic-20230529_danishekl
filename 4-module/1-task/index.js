8ef0f2dd80e16c7ee58b1b3b27eed0f72721eb3a
Lyubov Danishek <122434359+Lyubov4@users.noreply.github.com>
2023-07-03 12:00:55 +0800

    Решила задачу 4_module 1_task

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  for (let i = 0; i < friends.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = friends[i].firstName + ' ' + friends[i].lastName;
+    ul.appendChild(li);
+  }
+  return ul;
 }
+
