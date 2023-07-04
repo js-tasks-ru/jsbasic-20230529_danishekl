fd60078c54121a5f31b648bab28a9d80cd673b77
Lyubov Danishek <122434359+Lyubov4@users.noreply.github.com>
2023-07-04 12:29:12 +0800

    Решила задачу 4_module 2_task

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,14 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  let rows = table.rows;
+
+  for (let i = 0; i < rows.length; i++) {
+    let row = rows[i];
+    let cell = row.cells[i];
+    cell.style.backgroundColor = 'red';
+  }
 }
+
+let table = document.querySelector('table');
+makeDiagonalRed(table);
+
+
